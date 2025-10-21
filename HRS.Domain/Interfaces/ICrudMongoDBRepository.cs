eecd731e27e90142bf375fc683d65cee58bd7f4f
kritty04 <krit1_c@hotmail.com>
2025-10-21 18:22:41 +0800

    Update Crud

--- HRS.Domain/Interfaces/ICrudMongoDBRepository.cs
+++ HRS.Domain/Interfaces/ICrudMongoDBRepository.cs
@@ -5,13 +5,13 @@ namespace HRS.Domain.Interfaces;
 
 public interface ICrudMongoDBRepository<T> where T : class
 {
-    Task<T?> GetByIdAsync(string id);
+    Task<T?> GetByIdAsync(object id);
     Task<IEnumerable<T>> GetAllAsync();
     Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
     Task AddAsync(T entity);
     Task AddRangeAsync(IEnumerable<T> entities);
-    Task UpdateAsync(T entity, string id);
-    Task RemoveAsync(string id);
-    Task RemoveRangeAsync(IEnumerable<string> ids);
-    Task UpdateStatusByOrderIdAsync(string orderId, RentalStatus status);
+    Task UpdateAsync(T entity, object id);
+    Task RemoveAsync(object id);
+    Task RemoveRangeAsync(IEnumerable<object> ids);
+    Task UpdateStatusByOrderIdAsync(object orderId, RentalStatus status);
 }
